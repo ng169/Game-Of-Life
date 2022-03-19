de68fabf5d87449e444721696e332cb468fd5bf1
ng169 <pokeakshay123@gmail.com>
2022-03-19 21:54:43 +0530

    Colored box outputs

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 #include <sys/select.h>
 #include <unistd.h>
 #include <time.h>
+
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RESET "\x1b[0m"
@@ -22,10 +23,10 @@ int main()
     system("clear");
     printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tCONWAY'S GAME OF LIFE\n");
     usleep(600000);
-    printf("Enter:\n1) Random input\n2) Input from file\n");
-    printf("Enter choice: ");
+    printf("\nEnter:\n1) Random input\n2) Input from file\n");
+    printf("\nEnter choice: ");
     scanf("%d", &choice);
-    printf("Enter number of rows and columns: ");
+    printf("\nEnter number of rows and columns: ");
     scanf("%d%d", &rows, &cols);
     switch (choice)
     {
@@ -72,16 +73,16 @@ void display()
 {
     int i, j;
     system("clear");
-    printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\tCONWAY'S GAME OF LIFE\n\n\n");
+    printf("\n\n\t\t\t\tCONWAY'S GAME OF LIFE\n\n\n");
     for (i = 0; i < rows; i++)
     {
-        printf("\t\t\t\t\t\t\t\t\t");
+        printf("\t\t\t");
         for (j = 0; j < cols; j++)
         {
             if (grid[i][j] == 1)
-                printf(ANSI_COLOR_GREEN "#   " ANSI_COLOR_RESET);
+                printf(ANSI_COLOR_GREEN "\u25A0  " ANSI_COLOR_RESET);
             else
-                printf(ANSI_COLOR_RED "-   " ANSI_COLOR_RESET);
+                printf(ANSI_COLOR_RED "\u25A0  " ANSI_COLOR_RESET);
         }
         printf("\n");
     }
